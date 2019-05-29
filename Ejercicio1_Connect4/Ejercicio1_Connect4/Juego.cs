5fd900009c2939816df68bc3141c41f3d21279a9
Miguel T. Moronta <miguel@moronta.com.do>
2019-05-29 14:38:05 -0400

    Se concluye el desarrollo del Connect-4
    Se agregan las funcionalidades de evaluar si una partida se ha concluido. Hay una condición de error cuando se evalúa si una moneda está o no consecutivamente conectada.

---
+++ Ejercicio1_Connect4/Ejercicio1_Connect4/Juego.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Connect4
+{
+    public class Juego
+    {
+        #region "Enums"
+        public enum eEstadoJuego
+        {
+            MostrandoMenu,
+            Jugando,
+            Animando,
+            JuegoFinalizado
+        }
+        #endregion
+
+        #region "Atributos"
+        public eEstadoJuego Estado { get; set; }
+        public bool TurnoPrimerJugador { get; set; }
+        public string SiguienteAccion { get; set; }
+        public int SiguienteColumna { get; set; }
+        public int ProximaFila { get; set; }
+        public const float GRAVEDAD = 9.8f;
+        public bool?[,] Tablero { get; set; }
+        public const string SPRITE1 = "a";
+        public const string SPRITE2 = "o";
+        #endregion
+
+        #region "Comportamientos"
+        public int ProximaFilaDisponible(int numeroColumna)
+        {
+            int filaActual = Tablero.GetLength(0);
+
+            while (filaActual > 0 && Tablero[filaActual - 1, numeroColumna] != null)
+                filaActual--;
+
+            return filaActual - 1;
+        }
+
+        public bool VerificarFinPartida(int f, int c)
+        {
+            int[] Consecutivo = new int[4];
+            int i, j, k;
+            int iMin = f - 3 < 0 ? 0 : f - 3;
+            int iMax = f + 3 >= Tablero.GetLength(0) ? Tablero.GetLength(0) - 1 : f + 3;
+            int jMin = c - 3 < 0 ? 0 : c - 3;
+            int jMax = c + 3 >= Tablero.GetLength(1) ? Tablero.GetLength(1) - 1 : c + 3;
+            int kMin = c - 3 < 0 ? 0 : c - 3;
+            int kMax = c + 3 >= Tablero.GetLength(1) ? Tablero.GetLength(1) - 1 : c + 3;
+
+            for (i = iMin, j = jMin, k = kMax; i <= iMax || j <= jMax || k >= kMin; i++, j++, k--)
+            {
+                // Horizontales:
+                if (j <= jMax && Tablero[f, j] == TurnoPrimerJugador)
+                    Consecutivo[0]++;
+                // Verticales:
+                if (i <= iMax && Tablero[i, c] == TurnoPrimerJugador)
+                    Consecutivo[1]++;
+
+                //var tmp = (from contador in Consecutivo
+                //where contador == 4
+                //select contador).Any();
+            }
+
+            for (i = f, j = c, k = c; i >= iMin || j >= jMin || k <= kMax; i--, j--, k++)
+            {
+                // Diagonal 1:
+                if (i >= iMin && j >= jMin && Tablero[i, j] == TurnoPrimerJugador)
+                    Consecutivo[2]++;
+                // Diagonal 2:
+                if (i >= iMin && k <= kMax && Tablero[i, k] == TurnoPrimerJugador)
+                    Consecutivo[3]++;
+            }
+            if (Consecutivo.Any(contador => contador == 4))
+                return true;
+
+            Consecutivo[2] = 0;
+            Consecutivo[3] = 0;
+
+            for (i = f, j = c, k = c; i <= iMax || j <= jMax || k >= kMin; i++, j++, k--)
+            {
+                // Diagonal 1:
+                if (i <= iMax && j <= jMax && Tablero[i, j] == TurnoPrimerJugador)
+                    Consecutivo[2]++;
+                // Diagonal 2:
+                if (i <= iMax && k >= kMin && Tablero[i, k] == TurnoPrimerJugador)
+                    Consecutivo[3]++;
+            }
+
+            if (Consecutivo.Any(contador => contador == 4))
+                return true;
+
+            return false;
+        }
+        #endregion
+    }
+}
---
+++ Ejercicio1_Connect4/Ejercicio1_Connect4/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_Connect4
+{
+    class Program
+    {
+        static Thread hiloCaptura;
+        static Juego juegoActual = new Juego();
+        static int[] posicionesColumnas = new int[7] { 1, 3, 5, 7, 9, 11, 13 };
+
+        static void InicializarJuego()
+        {
+            juegoActual.Estado = Juego.eEstadoJuego.MostrandoMenu;
+            juegoActual.TurnoPrimerJugador = true;
+            juegoActual.SiguienteColumna = -1;
+            juegoActual.Tablero = new bool?[6, 7];
+            Console.CursorVisible = false;
+            hiloCaptura = new Thread(new ThreadStart(CapturarTeclado));
+            hiloCaptura.Start();
+        }
+
+        static void Main(string[] args)
+        {
+            InicializarJuego();
+
+            while(juegoActual.Estado != Juego.eEstadoJuego.JuegoFinalizado)
+            {
+                // Primero: verificamos la entrada del usuario.
+                
+                if(juegoActual.Estado == Juego.eEstadoJuego.Jugando && juegoActual.SiguienteColumna >= 0)
+                {
+                    juegoActual.Estado = Juego.eEstadoJuego.Animando;
+
+                    juegoActual.ProximaFila = juegoActual.ProximaFilaDisponible(juegoActual.SiguienteColumna);
+
+                    Thread nuevoHilo = new Thread(new ThreadStart(AnimarNuevaMoneda));
+                    nuevoHilo.Start();
+                    nuevoHilo.Join();
+                    
+                    juegoActual.Tablero[juegoActual.ProximaFila, juegoActual.SiguienteColumna] = juegoActual.TurnoPrimerJugador;
+                                        
+                    if (juegoActual.VerificarFinPartida(juegoActual.ProximaFila, juegoActual.SiguienteColumna))
+                    {
+                        juegoActual.Estado = Juego.eEstadoJuego.JuegoFinalizado;
+                        Console.SetCursorPosition(8, 5);
+                        Console.WriteLine("Ha ganado el jugador " + (juegoActual.TurnoPrimerJugador ? Juego.SPRITE1 : Juego.SPRITE2));
+                    }
+                        
+
+                    juegoActual.SiguienteColumna = -1;
+                    juegoActual.TurnoPrimerJugador = !juegoActual.TurnoPrimerJugador;
+                }
+
+                // Segundo: actualizamos valores en función del estado actual.
+
+                // Tercero: renderizamos el juego.
+                Renderizar();
+                Thread.Sleep(50);
+            }
+
+            Console.WriteLine("¡Gracias por jugar!");
+
+            Console.ReadKey();
+        }
+
+        private static void AnimarNuevaMoneda()
+        {
+            TimeSpan horaInicio = new TimeSpan(DateTime.Now.Ticks);
+            short posAnterior, posInicial = 5, posActual, posFinal;
+            posAnterior = posInicial;
+            posFinal = (short)(juegoActual.ProximaFila + 1);
+
+            if (posFinal < 1)
+                return;
+
+            do
+            {
+                posActual = Convert.ToInt16(posInicial + Juego.GRAVEDAD * Math.Pow((new TimeSpan(DateTime.Now.Ticks).Subtract(horaInicio).TotalSeconds), 2) / 2);
+
+                Console.SetCursorPosition(posicionesColumnas[juegoActual.SiguienteColumna], posAnterior);
+                Console.Write(" "); // Eliminamos la moneda anterior.
+
+                Console.SetCursorPosition(posicionesColumnas[juegoActual.SiguienteColumna], posActual);
+                Console.Write(juegoActual.TurnoPrimerJugador ? Juego.SPRITE1 : Juego.SPRITE2);
+
+                posAnterior = posActual;
+            } while (posActual <= (posInicial + posFinal));
+
+            juegoActual.Estado = Juego.eEstadoJuego.Jugando;
+        }
+
+        static void Renderizar()
+        {
+            switch(juegoActual.Estado)
+            {
+                case Juego.eEstadoJuego.MostrandoMenu:
+                    Console.SetCursorPosition(0, 0);
+                    Console.WriteLine("Seleccione una opción del menú: ");
+                    Console.WriteLine("\n\t1: Jugar.");
+                    Console.WriteLine("\t2: Salir.");
+                    break;
+                case Juego.eEstadoJuego.Jugando:
+                    ImprimirEscena();
+                    ImprimirTablero();
+                    break;
+            }
+        }
+
+        static void CapturarTeclado()
+        {
+            int _tmp;
+            string capturaActual;
+            while(true)
+            {
+                capturaActual = Console.ReadLine();
+                switch(juegoActual.Estado)
+                {
+                    case Juego.eEstadoJuego.MostrandoMenu:
+                        if (capturaActual == "2")
+                        {
+                            juegoActual.Estado = Juego.eEstadoJuego.JuegoFinalizado;
+                            Thread.CurrentThread.Abort();
+                        }
+                        else if (capturaActual == "1")
+                        {
+                            juegoActual.Estado = Juego.eEstadoJuego.Jugando;
+                        }
+                        break;
+                    case Juego.eEstadoJuego.Jugando:
+                        if(Int32.TryParse(capturaActual, out _tmp) && _tmp >= 1 && _tmp <= 7)
+                        {
+                            // Seleccionó una columna.
+                            juegoActual.SiguienteColumna = _tmp - 1;
+                        }
+                        break;
+                }
+            }
+        }
+
+        static void ImprimirEscena()
+        {
+            Console.Clear();
+            Console.WriteLine("CONNECT-4");
+            Console.WriteLine("Turno Actual: " + (juegoActual.TurnoPrimerJugador ? "Primer Jugador" : "Segundo Jugador"));
+            Console.WriteLine("\nSeleccione una columna [1-7]");
+            Console.WriteLine(" 1 2 3 4 5 6 7");
+            Console.WriteLine("--------------");
+            Console.WriteLine("| | | | | | | |");
+            Console.WriteLine("| | | | | | | |");
+            Console.WriteLine("| | | | | | | |");
+            Console.WriteLine("| | | | | | | |");
+            Console.WriteLine("| | | | | | | |");
+            Console.WriteLine("| | | | | | | |");
+            Console.WriteLine("--------------");
+        }
+
+        static void ImprimirTablero()
+        {
+            short posInicial = 6;
+
+            for(int i = 0; i < juegoActual.Tablero.GetLength(0); i++)
+            {
+                for(int j = 0; j < juegoActual.Tablero.GetLength(1); j++)
+                {
+                    if (juegoActual.Tablero[i, j] == null)
+                        continue;
+
+                    Console.SetCursorPosition(posicionesColumnas[j], posInicial + i);
+                    Console.Write(juegoActual.Tablero[i,j] == true ? Juego.SPRITE1 : Juego.SPRITE2);
+                }
+            }
+        }
+    }
+}
